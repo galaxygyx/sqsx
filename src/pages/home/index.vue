a7f3a9e2ad8fb8951088da5707f392a8b6cde71c
gaoyuxiang <yuxiang.gao@zhangmen.com>
2020-05-28 00:18:41 +0800

    home

---
+++ src/pages/home/index.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="home-wrapper">
+    <div class="header-box">
+      <div class="circle">消息</div>
+      <div class="circle">话题</div>
+    </div>
+    <div class="square-list">
+      <ul>
+        <li v-for="item in squareList" :key="item.id">
+          <div class="list-header">
+            <div class="user-info">
+              <img :src="item.wxavatar" alt="" class="avatar" />
+              <span class="name">{{ item.wxName }}</span>
+              <span class="flag">{{ item.flag }}</span>
+            </div>
+            <div class="date">{{ item.date }}</div>
+          </div>
+          <div class="list-content">
+            <p>{{ item.content }}</p>
+            <div class="img-list">
+              <img :src="src" alt="" v-for="(src, index) in imgList" :key="index" />
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      squareList: [
+        {
+          id: 1,
+          wxavatar: '../../static/logo.png',
+          wxName: '天下第一',
+          flag: '返乡拼车',
+          date: '2020/5/20  12:00',
+          content: '从太原→临县私家车，还有2个空位子，有需',
+          imgList: [
+            '../../static/logo.png',
+            '../../static/logo.png',
+            '../../static/logo.png'
+          ]
+        },
+        {
+          id: 2,
+          wxavatar: '../../static/logo.png',
+          wxName: '天下第一',
+          flag: '返乡拼车',
+          date: '2020/5/20  12:00',
+          content: '从太原→临县私家车，还有2个空位子，有需',
+          imgList: [
+            '../../static/logo.png',
+            '../../static/logo.png',
+            '../../static/logo.png'
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.home-wrapper {
+  .header-box {
+    background-color: #fff;
+    display: flex;
+    height: 152rpx;
+    justify-content: space-around;
+    align-items: center;
+    .circle {
+      background-color: #0074cd;
+      border-radius: 50%;
+      color: #fff;
+      font-size: 28rpx;
+      height: 94rpx;
+      line-height: 94rpx;
+      text-align: center;
+      width: 94rpx;
+      &:last-child {
+        background-color: #fc2b73;
+      }
+    }
+  }
+  .square-list {
+    li {
+      background-color: #fff;
+      padding: 40rpx;
+      margin-top: 20rpx;
+      overflow: hidden;
+    }
+    .list-header {
+      display: flex;
+      justify-content: space-between;
+    }
+    .user-info {
+      align-items: center;
+      display: flex;
+      justify-content: flex-start;
+      .avatar {
+        height: 90rpx;
+        width: 90rpx;
+      }
+      .name {
+        color: #5f5f5f;
+        font-size: 28rpx;
+        margin: 0 30rpx 0 15rpx;
+      }
+      .flag {
+        border: 2rpx solid #ff0000;
+        border-radius: 40rpx;
+        color: #ff0000;
+        font-size: 24rpx;
+        height: 40rpx;
+        line-height: 40rpx;
+        padding: 0 26rpx;
+      }
+    }
+    .list-content {
+      margin-left: 90rpx;
+      .img-list {
+        display: flex;
+        justify-content: space-between;
+        img {
+          height: 146rpx;
+          width: 146rpx;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,76 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <div class="index-wrapper">
+    <home v-if="actived === 1"></home>
+    <div class="tab-bar">
+      <span @click="handleClick(1)" :class="{ actived: actived === 1 }"></span>
+      <span @click="handleClick(2)" :class="{ actived: actived === 2 }"></span>
+    </div>
+    <div class="deciair-button-wrapper">
+      <img src="../../static/img/fabu.png" alt="" />
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+import Home from '../home/index'
+export default {
+  data() {
+    return {
+      actived: 1
+    }
+  },
+  components: { Home },
+  onLoad() {},
+  methods: {
+    handleClick(index) {
+      this.actived = index
+    }
+  }
+}
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+<style lang="scss">
+.index-wrapper {
+  .tab-bar {
+    align-items: center;
+    background-color: #f4f4f4;
+    bottom: 0;
+    display: flex;
+    height: 100rpx;
+    justify-content: space-around;
+    position: fixed;
+    width: 100%;
+    span {
+      height: 68rpx;
+      width: 68rpx;
+      &:first-child {
+        background: #f4f4f4 url('~@/static/img/home_default.png') no-repeat
+          center/90%;
+        &.actived {
+          background: #f4f4f4 url('~@/static/img/home_actived.png') no-repeat
+            center/90%;
+        }
+      }
+      &:last-child {
+        background: #f4f4f4 url('~@/static/img/my_default.png') no-repeat
+          center/90%;
+        &.actived {
+          background: #f4f4f4 url('~@/static/img/my_actived.png') no-repeat
+            center/90%;
+        }
+      }
+    }
+  }
+  .deciair-button-wrapper {
+    bottom: 47rpx;
+    left: 50%;
+    position: fixed;
+    transform: translateX(-50%);
+    z-index: 1000;
+    img {
+      height: 94rpx;
+      width: 94rpx;
+    }
+  }
+}
 </style>
